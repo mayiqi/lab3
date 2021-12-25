249e36b31b422542d8f7cc7b345994deeed949bd
mayiqi <94878682+mayiqi@users.noreply.github.com>
2021-12-25 22:48:59 +0800

    Add files via upload

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. lab3 documentation master file, created by
+   sphinx-quickstart on Sat Dec 25 22:44:49 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to lab3's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ services.py
@@ -0,0 +1,59 @@
+# Software Architecture and Design Patterns -- Lab 3 starter code
+# An implementation of the Service Layer
+# Copyright (C) 2021 Hui Lan
+
+
+# word and its difficulty level
+WORD_DIFFICULTY_LEVEL = {'starbucks':5, 'luckin':4, 'secondcup':4, 'costa':3, 'timhortons':3, 'frappuccino':6}
+
+#import pytest
+#import math
+import model
+
+class UnknownUser(Exception):
+    pass
+
+
+class NoArticleMatched(Exception):
+    pass
+
+
+def read(user, user_repo, article_repo, session):
+    u = user_repo.get(user.username)
+    if u == None or u.password != user.password:
+        raise UnknownUser()
+
+    articles = article_repo.list()
+    
+    if articles == None:
+        raise NoArticleMatched()
+        
+    words = session.execute(
+        'SELECT word FROM newwords WHERE username=:username',
+        dict(username=user.username),
+    )
+
+    sum = 0
+    count = 0
+    for word in words:
+        sum += WORD_DIFFICULTY_LEVEL[word[0]]
+        count += 1
+    
+    if count == 0:
+        count = 1
+
+    average = round(sum / count) + 1
+    if average < 3:
+        average = 3
+    
+    for article in articles:
+        if average == article.level:
+            article_id = user.read_article(article)
+            session.add(model.Reading(username = user.username, article_id = article_id))
+            session.commit()
+            return article_id
+
+    raise NoArticleMatched()
+
+
+
